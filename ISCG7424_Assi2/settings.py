9b4078ccd5196d19c83bfc4980e58809d1f1b073
apple <xsherron126@gmail.com>
2024-06-25 14:54:34 +1200

    django

--- ISCG7424_Assi2/settings.py
+++ ISCG7424_Assi2/settings.py
@@ -85,18 +85,30 @@ REST_FRAMEWORK = {
     ),
 }
 
-WSGI_APPLICATION = 'ISCG7424_Assi2.wsgi.application'
+# WSGI_APPLICATION = 'ISCG7424_Assi2.wsgi.application'
+WSGI_APPLICATION = 'ISCG7424_Assi2.wsgi.app'
 
 
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'verceldb',
+        'USER': 'default',
+        'PASSWORD': 'UFViRS1gxl2M',
+        'HOST': 'ep-super-night-a4da0q8u.us-east-1.aws.neon.tech',
+        'PORT': '5432',
     }
 }
+
 
 
 # Password validation
